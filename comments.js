d27590bc7e638ca8cd8139e18fa30f4bab29e723
Rafael Silva <81942196+devsilvarafael@users.noreply.github.com>
2023-11-07 11:47:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,11 @@
-// Create Web Server
-const http = require("http");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+import { createServer } from 'http';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!\n");
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, world!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
